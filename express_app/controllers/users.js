fbe4d4ca220bd365c6a9c62bc46dec02b50110c9
dans300 <danielsierramejia@gmail.com>
2024-10-16 00:13:45 +0200

    added the function to changename , it works

--- express_app/controllers/users.js
+++ express_app/controllers/users.js
@@ -56,36 +56,25 @@ app.post('/users',jsonParser,function (req, res)  {
         res.status(500).send("Erreur lors de l'ajout de l'utilisateur");
       });
   });
-  /*
-  app.post('/users', (req, res) => {
-    console.log('Corps de la requête :', req.body); // Pour déboguer
-    
-    const user = new User({
-      name: req.body.name,
-      email: req.body.email
-    });
-  
-    user.save()
-      .then(user => res.send(user))
-      .catch(error => {
-        console.log('Erreur lors de l\'ajout de l\'utilisateur :', error);
-        res.status(500).send("Erreur lors de l'ajout");
-      });
-  });
-  
-    
-     // Route pour ajouter John
-     app.post('/users/add-john', (req, res) => {
-        const user = new User({
-            name: 'John Doe',
-            email: 'john@doe.com'
-        });
+ 
 
-        user.save()
-        .then(() => res.send("John ajouté avec succès"))
-        .catch(error => {
-            console.log(error);
-            res.status(500).send("Erreur lors de l'ajout");
-        });
-    });*/
-};
+  
+
+
+   
+  app.post('/changeName',jsonParser,function (req, res)  {
+    User.findOneAndUpdate({name: req.body.name},
+                         { $set: { name: req.body.newName } })
+    .then(result => {
+      console.log(result+" nein");
+      return res.status(404).send({ message: 'User not found'});
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  })
+
+
+    }
+
+    
